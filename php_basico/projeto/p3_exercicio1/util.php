3062662eb760de22ee9b9e5142e80c54e629dd9a
Antonio Gutierrez <toinhoitacity@yahoo.com.br>
2015-12-05 11:17:51 -0200

    Projeto 3

--- php_basico/projeto/p3_exercicio1/util.php
+++ php_basico/projeto/p3_exercicio1/util.php
@@ -3,6 +3,7 @@ function procura_administrador($array, $user, $password){
     $achou = false;
     foreach($array as $admin){
         if($admin["usuario"] == $user && $admin["senha"] == $password){
+            return true;
             break;
         }
     }

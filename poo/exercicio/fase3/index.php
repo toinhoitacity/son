f4c00295a2e59b40ce0828e2fc1297cbd4899c03
Antonio Gutierrez <gutierrez.computacao@gmail.com>
2016-01-04 10:57:21 -0200

    Fase 3 exercicios

--- poo/exercicio/fase3/index.php
+++ poo/exercicio/fase3/index.php
@@ -2,7 +2,7 @@
 define('CLASS_DIR', 'poo/exercicio/fase3/src/');
 set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
 echo get_include_path();
-exit();
+//exit();
 spl_autoload_register();
 
 $x = new SON\Conta\Type\Conta();
